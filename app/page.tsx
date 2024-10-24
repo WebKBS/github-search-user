e6bf0d6793a6816d7368b30cb2bd99326189ad2e
kbs21c1987 <kbs21c1987@gmail.com>
2024-10-24 17:35:17 +0900

    feat: github 데이터 fetch logic 추가 및 타입 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import Section from "@/components/layouts/Section";
 import SearchInput from "@/components/inputs/SearchInput";
+import { getSearchedUsers, getSearchUserList } from "@/data/usersData";
 
-export default function Home() {
+export default async function Home() {
+  const data = await getSearchedUsers("kim");
+  console.log(data);
+
   return (
     <main>
+      {data.items.map((user) => (
+        <div key={user.id}>
+          <img src={user.avatar_url} alt={user.login} />
+          <p>{user.login}</p>
+        </div>
+      ))}
+
       <Section className="py-[80px]">
         <h1 className="text-3xl font-bold text-center mb-6">
           Try searching for Github users.
